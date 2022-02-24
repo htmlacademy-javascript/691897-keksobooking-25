553323576f7574ffc32303e0c8c12337b92dd818
Keks <keks@htmlacademy.ru>
2022-02-24 17:30:15 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -7,7 +7,7 @@ function getRandomIntInclusive(min, max) {
   if (min <= max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-    return false;
+  return false;
 }
 
 getRandomIntInclusive(1, 10);
@@ -19,7 +19,7 @@ function getRandomFloat(min, max, digits) {
   if ((min >= 0) && (min <= max)) {
     return +(Math.random() * (max - min + 1) + min).toFixed(digits);
   }
-    return false;
+  return false;
 }
 
 getRandomFloat(1, 3, 2);
